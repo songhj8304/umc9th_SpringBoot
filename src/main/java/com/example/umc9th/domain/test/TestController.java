c4d4fd345b5eef4c3a958942447b8b55108494e6
HyeJu Song <songhj8304@gmail.com>
2025-09-28 11:46:19 +0900

    ✨ feat : 응답 통일

--- src/main/java/com/example/umc9th/domain/test/TestController.java
+++ src/main/java/com/example/umc9th/domain/test/TestController.java
@@ -1,7 +1,10 @@
 package com.example.umc9th.domain.test;
 
+import com.example.umc9th.global.apiPayload.ApiResponse;
+import com.example.umc9th.global.apiPayload.code.GeneralSuccessCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +18,13 @@ public class TestController {
             description = "요청을 보내면 Hello World를 응답으로 보냅니다."
     )
 
-    public String test() {
+    /*public String test() {
         return "Hello World";
+    }*/
+
+    public ResponseEntity<ApiResponse<String>> test() {
+        return ResponseEntity.ok(
+                ApiResponse.onSuccess(GeneralSuccessCode.OK, "Hello World")
+        );
     }
 }
---
+++ src/main/java/com/example/umc9th/global/apiPayload/ApiResponse.java
@@ -0,0 +1,48 @@
+package com.example.umc9th.global.apiPayload;
+
+import com.example.umc9th.global.apiPayload.code.BaseErrorCode;
+import com.example.umc9th.global.apiPayload.code.BaseSuccessCode;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+
+public class ApiResponse<T> {
+
+    @JsonProperty("isSuccess")
+    private Boolean isSuccess;
+
+    @JsonProperty("code")
+    private String code;
+
+    @JsonProperty("message")
+    private String message;
+
+    @JsonProperty("result")
+    private T result;
+
+    public static <T> ApiResponse<T> onSuccess(BaseSuccessCode successCode, T result) {
+        return new ApiResponse<>(
+                true,
+                        successCode.getCode(),
+                        successCode.getMessage(),
+                        result
+        );
+    }
+
+    // 실패 응답
+    public static <T> ApiResponse<T> onFailure(BaseErrorCode errorCode, T result) {
+        return new ApiResponse<>(
+                false,
+                errorCode.getCode(),
+                errorCode.getMessage(),
+                result
+        );
+    }
+
+}
