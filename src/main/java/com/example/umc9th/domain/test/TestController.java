397ce68543c34ad1feedd8a1ad4867cd57190ac0
HyeJu Song <songhj8304@gmail.com>
2025-09-28 09:58:03 +0900

    ✨ feat 엔티티 생성

---
+++ src/main/java/com/example/umc9th/domain/test/TestController.java
@@ -0,0 +1,21 @@
+package com.example.umc9th.domain.test;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Tag(name = "테스트 API")
+public class TestController {
+
+    @GetMapping("/test")
+    @Operation(
+            summary = "테스트하는 API",
+            description = "요청을 보내면 Hello World를 응답으로 보냅니다."
+    )
+
+    public String test() {
+        return "Hello World";
+    }
+}
