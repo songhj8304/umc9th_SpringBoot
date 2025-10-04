204c899501c2f91534c1ad8a6f7a1518f9eb26e0
HyeJu Song <songhj8304@gmail.com>
2025-10-05 01:44:39 +0900

    🔨 refactor: term 엔티티 수정

--- src/main/java/com/example/umc9th/domain/term/entity/Term.java
+++ src/main/java/com/example/umc9th/domain/term/entity/Term.java
@@ -3,7 +3,14 @@ package com.example.umc9th.domain.term.entity;
 import com.example.umc9th.domain.term.enums.TermName;
 import com.example.umc9th.global.entity.BaseEntity;
 import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Table(name = "term")
 public class Term extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
